18e396a82a1c653f4dd0b77ffaf91c4ee10bc927
LeahWoodard <43422914+LeahWoodard@users.noreply.github.com>
2021-04-25 22:21:57 -0500

    Add rate movies column to movies.php
    base form of potential ratings format

--- 470Project/front-end/movies.php
+++ 470Project/front-end/movies.php
@@ -9,7 +9,7 @@ try{
 	/*$displayMovies ='select movie_title, movie_description, director_name, year_published, movie_duration
 						from movies
 						INNER JOIN directors ON movies.movie_director_id = directors.director_id;';*/
-	$displayMovies = 'select movie_title, genre_name, movie_description, director_name, year_published, movie_duration,GROUP_CONCAT(genre_name) AS genre
+	$displayMovies = 'select movie_id, movie_title, genre_name, movie_description, director_name, year_published, movie_duration,GROUP_CONCAT(genre_name) AS genre
 						from movies
 						INNER JOIN directors ON movies.movie_director_id = directors.director_id
 						INNER JOIN movie_genre ON movies.movie_id = movie_genre.movie_id_fk
@@ -44,6 +44,7 @@ try{
 <th>Movie Director</th>
 <th>Year</th>
 <th>Duration</th>
+<th>Rate Movie</th>
 </tr>
 </thead>
 <tbody>
@@ -55,6 +56,11 @@ try{
 <td><?php echo htmlspecialchars($row['director_name'])?></td>
 <td><?php echo htmlspecialchars($row['year_published'])?></td>
 <td><?php echo htmlspecialchars($row['movie_duration'])?></td>
+<td>
+	<form><input type="text" id=<?php $row['movie_id'] ?> name="rating" size="2" maxlength="2"><br><br></form>
+	<input type="submit" value="Submit">
+</td>
+  
 </tr>
 <?php endforeach; ?>
 </tbody>
