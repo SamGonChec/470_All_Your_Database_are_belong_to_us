fb9546bf6bf0d7bf1fbe3b821df0f86859afe237
SamGonChec <samuel.gonzalezcheca@outlook.com>
2021-04-28 13:09:34 -0500

    Additional Stored SQL files

--- 470Project/Files/Schema/GetRatings_StoredProcedure.sql
+++ 470Project/Files/Schema/GetRatings_StoredProcedure.sql
@@ -2,6 +2,7 @@ DELIMITER $$
 CREATE PROCEDURE GetRatings()
 BEGIN
 	SELECT movie_title, teen_rating, adult_rating, elder_rating, men_rating, women_rating
-		FROM ratings
-		INNER JOIN movies ON ratings.movie_id_fk = movies.movie_id;
+						from ratings
+						INNER JOIN movies ON ratings.movie_id_fk = movies.movie_id;
+                        
 END$$
--- 470Project/Files/Schema/Trigger_Inserting_Duplicate_Movie.sql
+++ 470Project/Files/Schema/Trigger_Inserting_Duplicate_Movie.sql
@@ -8,3 +8,8 @@ IF(EXISTS(SELECT 1 FROM movies WHERE movie_title = NEW.movie_title)) THEN
 SIGNAL SQLSTATE VALUE '45000' SET MESSAGE_TEXT = 'you cant insert a duplicate title';
 END IF;
 END$$
+
+INSERT INTO movies (movie_title, movie_description, movie_director_id, year_published, movie_duration)
+VALUES ('Split', 'scary', 1,2015,120)
+
+SELECT * FROM actors;
