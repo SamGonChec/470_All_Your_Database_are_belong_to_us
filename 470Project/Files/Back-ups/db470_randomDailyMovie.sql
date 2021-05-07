fbf02ede98541227065f722030e6d63a95497a04
SamGonChec <samuel.gonzalezcheca@outlook.com>
2021-05-06 21:21:18 -0500

    additional files

--- 470Project/Files/Back-ups/db470_randomDailyMovie.sql
+++ 470Project/Files/Back-ups/db470_randomDailyMovie.sql
@@ -24,30 +24,32 @@ SET @@SESSION.SQL_LOG_BIN= 0;
 SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
 
 --
--- Table structure for table `genres`
+-- Table structure for table `randomDailyMovie`
 --
 
-DROP TABLE IF EXISTS `genres`;
+DROP TABLE IF EXISTS `randomDailyMovie`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `genres` (
-  `genre_id` int NOT NULL AUTO_INCREMENT,
-  `genre_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`genre_id`),
-  UNIQUE KEY `category_id` (`genre_id`),
-  UNIQUE KEY `genre_name` (`genre_name`),
-  UNIQUE KEY `genre_name_2` (`genre_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `randomDailyMovie` (
+  `rmovie_id` int NOT NULL,
+  `rmovie_title` varchar(50) NOT NULL,
+  `rmovie_description` text NOT NULL,
+  `rmovie_director` varchar(50) NOT NULL,
+  `ryear_published` year NOT NULL,
+  `rmovie_duration` int NOT NULL,
+  `rmovie_genre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`rmovie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `genres`
+-- Dumping data for table `randomDailyMovie`
 --
 
-LOCK TABLES `genres` WRITE;
-/*!40000 ALTER TABLE `genres` DISABLE KEYS */;
-INSERT INTO `genres` VALUES (1,'Action'),(2,'Adventure'),(11,'Biography'),(7,'Comedy'),(14,'Crime'),(18,'Documentary'),(6,'Drama'),(17,'Family'),(5,'Fantasy'),(9,'History'),(13,'Horror'),(8,'Music'),(3,'Mystery'),(10,'Romance'),(4,'Sci-Fi'),(16,'Sport'),(12,'Thriller'),(15,'War');
-/*!40000 ALTER TABLE `genres` ENABLE KEYS */;
+LOCK TABLES `randomDailyMovie` WRITE;
+/*!40000 ALTER TABLE `randomDailyMovie` DISABLE KEYS */;
+INSERT INTO `randomDailyMovie` VALUES (210,'The Girl with All the Gifts','\"A scientist and a teacher living in a dystopian future embark on a journey of survival with a special young girl named Melanie.\"','Colm McCarthy',2016,111,'Drama');
+/*!40000 ALTER TABLE `randomDailyMovie` ENABLE KEYS */;
 UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +62,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-06 20:56:01
+-- Dump completed on 2021-05-06 20:55:41
--- 470Project/Files/Back-ups/db470_visitor_ratings.sql
+++ 470Project/Files/Back-ups/db470_visitor_ratings.sql
@@ -31,13 +31,14 @@ DROP TABLE IF EXISTS `visitor_ratings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `visitor_ratings` (
-  `rating_id` int NOT NULL,
+  `rating_id` int NOT NULL AUTO_INCREMENT,
   `movie_id` int NOT NULL,
   `rating` double DEFAULT NULL,
   PRIMARY KEY (`rating_id`),
   KEY `fk_visitor_ratings_movies` (`movie_id`),
-  CONSTRAINT `fk_visitor_ratings_movies` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `fk_visitor_ratings_movies` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`),
+  CONSTRAINT `Check_Rating` CHECK (((`rating` >= 1) and (`rating` <= 10)))
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,6 +47,7 @@ CREATE TABLE `visitor_ratings` (
 
 LOCK TABLES `visitor_ratings` WRITE;
 /*!40000 ALTER TABLE `visitor_ratings` DISABLE KEYS */;
+INSERT INTO `visitor_ratings` VALUES (1,2,2),(2,1,6),(3,3,4),(4,4,2.2),(5,5,10),(6,9,1),(7,2,1),(8,2,1),(9,1,1),(10,9,10),(11,2,7),(12,2,7),(13,2,7),(14,2,7),(15,10,1),(16,13,5),(17,4,7),(18,1,5),(19,4,4),(20,4,3);
 /*!40000 ALTER TABLE `visitor_ratings` ENABLE KEYS */;
 UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
@@ -59,4 +61,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-14 11:07:23
+-- Dump completed on 2021-05-06 20:56:04
